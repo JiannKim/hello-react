b1c166a6fbdb85bae660f2a8bb9dfd185728f1fb
kim jiahn <kimjiahn@kimui-MacBookPro.local>
2021-04-30 16:22:17 +0900

    :memo: 2장 JSX - 2.4.4
    2.4.4 && 연산자조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // 아래와 같이 코드를 작성하면 아무것도 화면에 렌더링되지 않는다
+  // 아무것도 보여주고 싶지 않을경우 아래 코드와같이 작성한다.
   // name 값을 'react'로 수정하면 "리액트입니다."가 출력 된다.
   // const name = "reeact";
-  // return <div>{name === 'react' && <h1>리액트입니다.</h1>}</div>;
-  // 아무것도 보여주고 싶지 않을경우 아래 코드와같이 작성한다
+  // return <div>{name === 'react' ? <h1>리액트입니다.</h1> : null}</div>;
+
+  // 아래 코드와 같이 작성하면 위에서 작성한 코드 보다 더 짧은 코드로 똑같은 작업을 할 수 있다.
   const name = "reeact";
   return <div>{name === 'react' && <h1>리액트입니다.</h1>}</div>;
-  // &&(and) 연산자로 조건부 렌더링을 할 수 있는 이유는 리액트에서 false를 랜더링 할 때는 null과 마찬가지로 아무것도 나타나지 않기 때문이다
+  // &&(and) 연산자로 조건부 렌더링을 할 수 있는 이유는 리액트에서 false를 랜더링 할 때는 null과 마찬가지로 아무것도 나타나지 않기 때문이다.
 }
 
 export default App;
