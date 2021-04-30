2782a8efe602fac84c4148f836e0f6a391f47dbb
kim jiahn <kimjiahn@kimui-MacBookPro.local>
2021-04-30 16:14:50 +0900

    :memo: 2장 JSX
    2.4.4 && 연산자를 사용한 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -2,30 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    // 하나의 요소로 반드시 감싸주어야한다.
-    // 이유는 Virtual DOM 에서 컴포넌트의 변화를 감지 할 때에 효율적으로 비교 할 수 있도록
-    // 컴포넌트 내부는 하나의 DOM 트리 구조로 이루어 져야한다는 규칙이 있다.
-    // div가 마음에 안든다면 react v16이상부터 도입된 <Fragment /> 를 사용 할 수 있다.
-    // <Fragment> or <>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    // </Fragment> or </>
-  );
+  // 아래와 같이 코드를 작성하면 아무것도 화면에 렌더링되지 않는다
+  // name 값을 'react'로 수정하면 "리액트입니다."가 출력 된다.
+  // const name = "reeact";
+  // return <div>{name === 'react' && <h1>리액트입니다.</h1>}</div>;
+  // 아무것도 보여주고 싶지 않을경우 아래 코드와같이 작성한다
+  const name = "reeact";
+  return <div>{name === 'react' && <h1>리액트입니다.</h1>}</div>;
+  // &&(and) 연산자로 조건부 렌더링을 할 수 있는 이유는 리액트에서 false를 랜더링 할 때는 null과 마찬가지로 아무것도 나타나지 않기 때문이다
 }
 
 export default App;
