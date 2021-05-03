90502c616e4c850490719468d1f3fd58524e205e
kim jiahn <kimjiahn@kimui-MacBookPro.local>
2021-05-03 11:51:19 +0900

    :memo: 3.2 첫 컴포넌트 생성
    컴포넌트를 생성 및 내보내기/불러오기 기능 사용

--- src/App.js
+++ src/App.js
@@ -1,49 +1,9 @@
-import { configure } from '@testing-library/dom';
-import { Component } from 'react';
 import './App.css';
+import MyComponent from './MyComponent';
 
-// 3장
-// 3.1 클래스형 컴포넌트
-class App extends Component {
-  render() {
-    const name = '리액트';
-    return (
-      <div className="react">
-        {name}
-        <input>this</input>
-      </div>
-    );
-  }
-}
-// 노트
-// ES6의 클래스 문법
-// es6 이전에는 js에 class가 없었다. 개념 자체는 있었지만, 그것을 구현하려면 class대신 prototype이라는 문법을 사용해야했다.
-// 아래 코드를 보자.
-function Dog(name) {
-  this.name = name;
-}
-Dog.prototype.say = function () {
-  console.log(this.name + ': 멍멍');
+// 3.2.3.2 불러오기(import)
+const App = () => {
+  return <MyComponent />;
 };
-var dog = new Dog('검둥이');
-dog.say(); // 검둥이: 멍멍
-
-// ES6 문법부터는 위와 기능이 똑같은 코드를 class를 사용해 아래와 같이 작성 할 수 있다.
-class Dog {
-  constructor(name) {
-    this.name = name;
-  }
-  say() {
-    console.log(this.name + ': 멍멍');
-  }
-}
-const dog = new Dog('흰둥이');
-dog.say(); // 흰둥이: 멍멍
-
-// 특징과 차이점 정리
-// - 클래스형 컴포넌트에서는 render 함수가 꼭 있어야하고, 그 안에 보여줄 JSX를 반환 해야한다.
-// - 함수형보다 메모리 자원을 더 많이 사용한다.
-// - state와 라이프 사이클 기능 사용이 가능하다. (v16.8 업뎃 이후 함수형 컴포넌트에 Hooks 기능도입으로 단점 해결)
-// - 임의 메서드를 정의 할 수 있다.
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,72 +1,9 @@
 import React from 'react';
 
 // 3.2 첫 컴포 생성
-// 3.2.2 코드 작성하기
+// 3.2.3 모듈 내보내기 및 불러오기(export & import )
 const MyComponent = () => {
   return <div>my new component</div>;
 };
-
-// 노트1
-// 화살표 함수는 ES6 문법에서 함수를 표현하는 새로운 방식이다.
-// 하지만 이것이 기존 function을 완전히 대체하지는 않는다.
-// 사용 용도가 조금 다른데, 주로 함수를 파라미터로 전달할 때 유용하다. 아래 코드를 보면서 이해해보자.
-setTimeout(function () {
-  console.log('hello world');
-}, 1000);
-
-setTimeout(() => {
-  console.log('hello world');
-}, 1000);
-
-// 이 문법이 기존 function을 대체 할 수 없는 것은 용도가 다르기 때문이다. 무엇보다 서로 가리키고 있는 this값이 다르다.
-// 아래 코드를 확인해보자.
-
-function BlackDog() {
-  this.name = '흰둥이';
-  return {
-    name: '검둥이',
-    bark: function () {
-      console.log(this.name + ': 멍멍');
-    },
-  };
-}
-const blackDog = new BlackDog();
-blackDog.dark(); // 검둥이: 멍멍
-
-function WhiteDog() {
-  this.name = '흰둥이';
-  return {
-    name: '검둥이',
-    dark: () => {
-      console.log(this.name + ': 멍멍');
-    },
-  };
-}
-const whiteDog = new WhiteDog();
-whiteDog.dark(); // 흰둥이: 멍멍
-
-// function()을 사용했을 때는 검둥이, () => 를 사용했을 때는 흰둥이가 나타난다.
-// 일반 함수는 자신이 종속된 객체를 this로 가르킨다.
-// 화살표 함수는 자신이 종속된 인스턴스를 가르킨다.
-// 화살표 함수는 값 연산 후 바로 반환해야 할 때 사용하면 가독성을 높일 수 있다.
-function twice(value) {
-  return value * 2;
-}
-const triple = (value) => value * 3;
-// 위 처럼 {}를 작성하지 않으면 연산값을 그대로 반환한다는 의미이다.
-
-// 노트2
-// 코드 스니펫을 사용하면 화살표 함수를 사용한 컴포넌트를 빠르게 작성 할 수 있다.
-// reactjs code snippet 확장 프로그램이 설치 되어있다면 rsc -> Enter 해서 빠르게 작성해보자.
-// 아래의 코드는 스니펫을 사용한 컴포넌트 이다.
-// import React from 'react';
-
-// const componentName = () => {
-//     return (
-//         <div>
-
-//         </div>
-//     );
-// };
-
-// export default componentName;
+// 다른 파일에서 이 파일을 불러와 사용 할 수 있도록 설정한다.
+export default MyComponent; // 3.2.3.1 내보내기(export)
