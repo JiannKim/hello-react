c4bb9007d70ac9adb3b0bbfef61c42be85366ddd
kim jiahn <kimjiahn@kimui-MacBookPro.local>
2021-05-03 19:45:11 +0900

    :memo: 3장 컴포넌트 - 3.4 state
    3.4.1.3 this.setState에 객체 대신 함수 인자 전달하기

--- src/Counter.js
+++ src/Counter.js
@@ -9,7 +9,7 @@ import React, { Component } from 'react';
 //  props를 바꾸려면 실제 사용 되는 부모 컴포에서 바꾸어야 한다.     //
 //------------------------------------------------------//
 
-// 리액트에는 두가지 종류의 state가 있다. 
+// 리액트에는 두가지 종류의 state가 있다.
 // - 클래스형 컴포넌트에서 사용하는 state
 // - 함수형 컴포넌트에서의 useState라는 함수를 통해 사용하는 state
 class Counter extends Component {
@@ -28,9 +28,33 @@ class Counter extends Component {
         <h2>바뀌지 않는 값: {fiexdNumber}</h2>
         <button
           style={{ cursor: 'pointer' }}
-          onClick={() => {  // onClick(이벤트) props는 클릭시 호출될 함수를 설정할 수 있게 해준다.
-            // this.setState를 사용하여 state에 새로운 값을 넣을 수 있다. (state값을 바꿀 수 있게 해줌)
-            this.setState({ number: number + 1 });  // setState 함수는 안자로 전달된 객체 안에 들어있는 값만 바꾼다.
+          onClick={() => {
+            // 3.4.1.3 this.setState에 객체 대신 함수 인자 전달하기
+            //-----------------------------------------------------------------------//
+            // this.setState를 사용하여 state값을 업데이트할 때는 비동기적으로 상태가 업데이트 된다. //
+            //-----------------------------------------------------------------------//
+            // 만일 아래 코드를 추가한다고 해도 버튼을 클릭할 때 숫자는 1씩 더해 진다.
+            // this.setState를 사용한다고 해서 state값이 바로 바뀌지는 않기 때문이다. (비동기)
+            // this.setState({nunber: this.state.number + 1});
+
+            // 이에 대한 해결책은 this.setState를 사용할 때 객체 대신 함수를 인자로 넣어주면 된다.
+            // 예: this.setState((prevState, props) => { // prevState -> 기존상태 , props -> 현재 지닌 props (이과정에서 props가 필요 없다면 생략 가능하다.)
+            
+            // this.setState((prevState) => {
+            //   return {
+            //     // 업데이트할 내용
+            //     number: prevState.number + 1,
+            //   };
+            // });
+
+            // 위 코드와 아래 코드는 완전히 똑같은 기능을 갖는다.
+            // 아래의 코드는 함수에서 바로 객체를 반환한다는 의미이다.
+            this.setState((prevState) => ({
+              number: prevState.number + 1,
+            }));
+
+            // 두 번째로 this.setState 함수를 사용할 때는 바로 객체를 반환하도록 했기 때문에 prevState => ({}) 형태로 작성되었다.
+            // 숫자는 2씩 증가한다.
           }}
         >
           +1
