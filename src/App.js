8671c2be7ff00270142bee71afe3035efc11f981
kim jiahn <kimjiahn@kimui-MacBookPro.local>
2021-05-03 14:48:23 +0900

    :memo: 3.3 props
    3.3.6 propTypes와 props 검증 & 더 많은 PropTypes 종류

--- src/App.js
+++ src/App.js
@@ -9,8 +9,20 @@ const App = () => {
   // 3.3.3 props 기본값 설정: defaultProps (* MyComponent.js 참조)
   // return <MyComponent />;
 
-  // 3.3.4 태그 사이 내용을 보여 주는 children (* MyComponent.js 참조)
-  return <MyComponent> 리액트 </MyComponent>;
+  // 3.3.6 propTypes를 통한 props 검증
+  // MyComponent에서 propTypes를 문자열로 받도록 설정하였다.
+  // 브라우저내에서는 정상적으로 렌더링되지만 검사에 콘솔로그를 보면 propTypes(props의 name)가 잘못되었다는 경고문구가 떠있다.
+  return (
+    // 3.3.6.1 isRequired를 사용하여 필수 propTypes 설정 (* MyComponent.js 참조)
+    // favoriteNumber까지 잘 렌더링된것을 볼 수 있다.
+    <MyComponent name="React" favoriteNumber={3}>
+      리액트
+    </MyComponent>
+  );
+
+  // 에러 메세지를 확인한 후 제대로된 name값을 작성한다. (문자열로)
+  // return <MyComponent name={3}> 리액트 </MyComponent>; <-- errer
+  // return <MyComponent name="React"> 리액트 </MyComponent>; <-- rendering
 };
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,24 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // 3.3 props
-// 3.3.5 비구조화 할당 문법을 통해 props 내부값 추출하기
-// const MyComponent = (props) => {
-//   // 비구조 할당
-//   const { name, children } = props;
-//   return (
-//     <div>
-//       안녕하세요. 저는 {name} 이예요. <br />
-//       저는 {children} 이예요.
-//     </div>
-//   );
-// };
-// 비구조 할당은 함수의 파라미터에서도 사용할 수 있다.
-// 코드가 간결해지고 사용하기 편하다.
-const MyComponent = ({ name, children }) => {
+// 3.3.6 propTypes를 통한 props 검증
+const MyComponent = ({ name, children, favoriteNumber }) => {
   return (
     <div>
       안녕하세요. 저는 {name} 이예요. <br />
       저는 {children} 이예요.
+      <br />
+      제가 좋아하는 숫자는 {favoriteNumber}예요.
     </div>
   );
 };
@@ -28,5 +19,32 @@ const MyComponent = ({ name, children }) => {
 MyComponent.defaultProps = {
   name: '기본 이름',
 };
+
+// 3.3.6 propTypes를 통한 props 검증 (* App.js 참조)
+// 아래와 같이 설정하면 name값은 무조건 문자열(string) 형태로 전달해야 된다는 것을 의미한다.
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  // 3.3.6.1 isRequired를 사용하여 필수 propTypes 설정
+  // propTypes이 잘못 되엇을 때 경고문구를 띄우도록 아래 코드와같이 작성하여 설정한다.
+  // 지정한 favoriteNumber를 사용하지 않았을때는 해당값이 'undefined'라는 별도의 에러메세지가 뜬다.
+  favoriteNumber: PropTypes.number.isRequired,
+};
+
+// 3.3.6.2 더 많은 PropTypes 종류
+// - array: 배열
+// - arrayOf(다른 PropTyep): 특정 PropType으로 이루어진 배열을 의미. (예: arrayOf(PropTypes.number)는 숫자로 이루어진 배열이다.)
+// - bool: true 혹은 false로 이루어진 값
+// - func: 함수
+// - number: 숫자
+// - object: 객체
+// - string: 문자열
+// - symbol: ES6의 Symbol
+// - node: 렌더링할 수 있는 모든 것(숫자, 문자열, 혹은 JSX 코드, children도 node PropType이다.)
+// - instanceOf(클래스): 특정 클래스의 인스턴스(예: instanceOf(MyClass))
+// - onbeforeprint(['dog', 'cat']): 주어진 배열 요소 중 값 하나
+// - oneOfType([React.PropTypes.string, PropTypes.number]): 주어진 배열 안의 종류 중 하나
+// - objectOf(React.PropTypes.number): 객체의 모든 키 값이 인자로 주어진 PropType인 객체
+// - shape({name: PropTypes.string, num: PropTypes.number}): 주어진 스키마를 가진 객체
+// - any: 아무 종류
 
 export default MyComponent;
