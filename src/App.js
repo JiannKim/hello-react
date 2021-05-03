ada78eb1dc5bfbf7fe77e95bfeb9c5c0a7504f6f
kim jiahn <kimjiahn@kimui-MacBookPro.local>
2021-05-03 12:14:53 +0900

    :memo: 3.3 props
    props 렌더링과 기본값 그리고 children값 렌더링

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import MyComponent from './MyComponent';
 
-// 3.2.3.2 불러오기(import)
+// 3.3.2 컴포넌트를 사용할 때 props값 지정하기
+// name 프로퍼티를 사용하여 props값('React')을 전달할 수 있도록 하였다.
 const App = () => {
-  return <MyComponent />;
+  // return <MyComponent name="React" />;
+
+  // 3.3.3 props 기본값 설정: defaultProps (* MyComponent.js 참조)
+  // return <MyComponent />;
+
+  // 3.3.4 태그 사이 내용을 보여 주는 children (* MyComponent.js 참조)
+  return <MyComponent> 리액트 </MyComponent>;
 };
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
-// 3.2 첫 컴포 생성
-// 3.2.3 모듈 내보내기 및 불러오기(export & import )
-const MyComponent = () => {
-  return <div>my new component</div>;
+// 3.3 props
+// properties를 줄인 표현으로 컴포넌트 속성을 설정할 때 사용하는 요소이다.
+// 해당 컴포를 불러와 사용하는 부모 컴포에서 설정할 수 있다. (현재 App <- 부모 컴포)
+
+// 3.3.1 JSX 내부에서 props 렌더링
+// 아래 코드에서 name props를 렌더링하도록 설정 하였다.
+const MyComponent = (props) => {
+  //   return <div>안녕하세요. 저는 {props.name} 이예요.</div>;
+  // };
+
+  // 3.3.3 props 기본값 설정: defaultProps (* App.js 참조)
+  // 기본값을 설정 할 때는 아래 코드와 같이 작성한다.
+  MyComponent.defaultProps = {
+    name: '기본 이름',
+  };
+
+  // 3.3.4 태그 사이 내용을 보여 주는 children (* App.js 참조)
+  // 태그 사이의 내용을 보여줄 때는 아래 코드 처럼 작성하면 부모에게서 받아온 props값을 렌더링한다.
+  return (
+    <div>
+      안녕하세요, 저는 {props.name}이예요. <br />
+      children 값은 {props.children}이예요.
+    </div>
+  );
 };
-// 다른 파일에서 이 파일을 불러와 사용 할 수 있도록 설정한다.
-export default MyComponent; // 3.2.3.1 내보내기(export)
+
+export default MyComponent;
