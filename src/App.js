ec6761291848035b279b7d63e0ed629fa4ffedb2
kim jiahn <kimjiahn@kimui-MacBookPro.local>
2021-05-03 10:04:14 +0900

    :memo: 2장 JSX - 2.4.7
    2.4.7 class 대신 className

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
 
-// 2.4.6 인라인 스타일링
+// 2.4.7 class 대신 className
+// react v16부터는 class를 className으로 변환시켜주고 경고를 띄운다.
 function App() {
-  const name = '리액트';
-  const style = {
-    backgroundColor: 'black', // background-color와 같은 경우 - 가 사라지고 카멜 표기법(camelCase)으로 작성한다.
-    color: 'aqua',
-    fontSize: '48px', // font-size -> fontSize
-    fontWeight: 'bold', // font-weight -> fontWeight
-    padding: 16 // 단위 생략시 px로 지정된다.
-  }
-  return <div style={style}>{name}</div>;
-  // 만일 스타일 객체를 미리 선언하지않고 바로 지정하고 싶다면 div의 style 값에 직접 지정하는 방법이 있다.
-  // ex) style = {{color: red, fontSize: 20}}
+  const name = '리액트'
+  return <div className='react'>{name}</div>
 }
 
 export default App;
