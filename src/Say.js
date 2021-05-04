2a74b2dbe98903c06b19b7b7cec9987c3d3e12af
JiannKim <jiahnkimm@gmail.com>
2021-05-04 10:28:02 +0900

    :memo: 3장 컴포넌트 - 3.4 state
    3.4.2.3 한 컴포넌트에서 useState 여러번사용하기

--- src/Say.js
+++ src/Say.js
@@ -20,12 +20,24 @@ const Say = () => {
   const [message, setMessage] = useState('');
   const onClickEnter = () => setMessage('안녕하세요.');
   const onClickLeave = () => setMessage('안녕히 가세요.');
+  
+  // 3.4.2.3 한 컴포넌트에서 useState 여러 번 사용하기
+  const [color, setColor] = useState('black');
 
   return (
     <div>
       <button onClick={onClickEnter}>입장</button>
       <button onClick={onClickLeave}>퇴장</button>
-      <h1>{message}</h1>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: 'red' }} onClick={() => setColor('red')}>
+        RED
+      </button>
+      <button style={{ color: 'green' }} onClick={() => setColor('green')}>
+        GREEN
+      </button>
+      <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+        BLUE
+      </button>
     </div>
   );
 };
