02bdb4189a8fd6ffd995e68eee6cd3a09414119a
JiannKim <jiahnkimm@gmail.com>
2021-05-04 15:41:31 +0900

    :memo: 4장 이벤트 핸들링 - 4.2.3 임의 메서드 만들기
    바벨의 transform-class-properties 문법을 사용하여 효율과 가독성 높이기

--- src/4장_이벤트핸들링/EventPractice.js
+++ src/4장_이벤트핸들링/EventPractice.js
@@ -12,33 +12,24 @@ class EventPractice extends Component {
   // 성능상으로는 차이가 거의 없지만, 가독성이 훨씬 좋다.
   // onChange와 onClick에 전달한 함수를 따로 빼내서 컴포넌트의 임의 메서드를 만들어 준다.
 
-  // 4.2.3.1 기본 방식
-  //-----------------------------------------------------------------------------//
-  //  함수가 호출될 때 this는 호출부에 따라 결정되기 때문에                                  //
-  //  클래스 임의 메서드가 특정 html 요소의 이벤트로 등록되는 과정에서                          //
-  //  메서드와 this의 관계가 끊어져 버린다.                                              //
-  //  이 때문에 임의 메서드가 이벤트로 등록되어도 this를 컴포넌트 자신으로 제대로 가리키기 위해서는    //
-  //  메서드를 this와 바인딩(binding)하는 작업이 필요하다.                                //
-  //  만약 바인딩하지 않는 경우라면 this가 undefined를 가리키게 된다.                       //
-  //----------------------------------------------------------------------------//
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleChange(e) {
+  // 4.2.3.2 Property Initializer Syntax를 사용한 메서드 작성
+  // 메서드 바인딩은 생성자 메서드에서 하는것이 정석이다.
+  // 하지만 이 작업을 불편하다고 느낄 수 있다.
+  // 새 메서드를 만들 때마다  constructor도 수정해야 하기 때문이다.
+  // 이 작업을 더 간단하게 하는 방법으로는 바벨의 transform-class-properties 문법을 사용하여
+  // 화살표 함수 형태로 메서드를 정의하는 것이다.
+  handleChange = (e) => {
     this.setState({
       message: e.target.value,
     });
-  }
+  };
 
-  handleClick(e) {
+  handleClick = (e) => {
     alert(this.state.message);
     this.setState({
       message: '',
     });
-  }
+  };
 
   render() {
     return (
