a3e60a0b43b48a148e3c5f296916ce33213afe1c
JiannKim <jiahnkimm@gmail.com>
2021-05-04 15:39:12 +0900

    :memo: 4장 이벤트 핸들링 - 4.2.3 임의 메서드 만들기
    클래스 임의 메서드 기본방식 만들고 알아보기

--- src/4장_이벤트핸들링/EventPractice.js
+++ src/4장_이벤트핸들링/EventPractice.js
@@ -6,9 +6,41 @@ class EventPractice extends Component {
   state = {
     message: '',
   };
+
+  // 4.2.3 임의 메서드 만들기
+  // 앞서 배운 방법 대신 함수를 미리 만들어놓고 전달하는 방법도 있다.
+  // 성능상으로는 차이가 거의 없지만, 가독성이 훨씬 좋다.
+  // onChange와 onClick에 전달한 함수를 따로 빼내서 컴포넌트의 임의 메서드를 만들어 준다.
+
+  // 4.2.3.1 기본 방식
+  //-----------------------------------------------------------------------------//
+  //  함수가 호출될 때 this는 호출부에 따라 결정되기 때문에                                  //
+  //  클래스 임의 메서드가 특정 html 요소의 이벤트로 등록되는 과정에서                          //
+  //  메서드와 this의 관계가 끊어져 버린다.                                              //
+  //  이 때문에 임의 메서드가 이벤트로 등록되어도 this를 컴포넌트 자신으로 제대로 가리키기 위해서는    //
+  //  메서드를 this와 바인딩(binding)하는 작업이 필요하다.                                //
+  //  만약 바인딩하지 않는 경우라면 this가 undefined를 가리키게 된다.                       //
+  //----------------------------------------------------------------------------//
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      message: e.target.value,
+    });
+  }
+
+  handleClick(e) {
+    alert(this.state.message);
+    this.setState({
+      message: '',
+    });
+  }
+
   render() {
-    // 4.2.2 onChange 이벤트 핸들링하기
-    // 메서드 내부에 input 요소를 렌더링하는 코드와 해당 요소에 onChange 이벤트를 설정하는 코드 작성
     return (
       <div>
         <h1>이벤트 실습</h1>
@@ -17,27 +49,9 @@ class EventPractice extends Component {
           name="message"
           placeholder="아무거나 입력..."
           value={this.state.message}
-          onChange={(e) => {
-            this.setState({
-              message: e.target.value,
-            });
-          }}
+          onChange={this.handleChange}
         />
-        {/* 
-        4.2.2.3 버튼을 누를 때 comment 값을 공백으로 설정 
-        클릭 이벤트가 발생하면 현재 comment값(state의 message)을 alert박스로 띄운 후
-        comment값을 공백으로 변경한다.
-        */}
-        <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({
-              message: '',
-            });
-          }}
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
