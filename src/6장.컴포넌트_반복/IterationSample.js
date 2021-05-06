3db656dfee93271c05ed650378c711601dd416eb
JiannKim <jiahnkimm@gmail.com>
2021-05-06 15:16:45 +0900

    :memo: 6장 컴포넌트 반복 - 6.3 key
    key 설정 & 고유한 값이 없을 때 map 함수 내부 콜백 함수의 인수인 index값을 사용하기

--- src/6장.컴포넌트_반복/IterationSample.js
+++ src/6장.컴포넌트_반복/IterationSample.js
@@ -5,7 +5,9 @@ import React from 'react';
 const IterationSample = () => {
   // 6.2 데이터 배열을 컴포넌트 배열로 변환하기
   const name = ['눈사람', '눈', '얼음', '바람'];
-  const nameList = name.map((name) => <li>{name}</li>);
+  // 6.3.1 key 설정
+  // 고유한 값이 없을 때만 index를 key로 사용해야 한다. 일반적으로 index를 사용하면 배열이 변경될 때 효율적으로 리렌더링 하지 못한다.
+  const nameList = name.map((name, index) => <li key={index}>{name}</li>);
   return <ul>{nameList}</ul>;
 };
 
