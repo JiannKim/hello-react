f2a89a0cff52a204802b11828ab19923c65b7ba7
JiannKim <jiahnkimm@gmail.com>
2021-05-31 16:08:47 +0900

    :memo: 8장 Hooks - 8.1 useState
    useState 사용하기와 여러번 사용하기

---
+++ hooks-turorial/src/8장.Hooks/step1.js
@@ -0,0 +1,8 @@
+// 8.1 useState (* Counter.js 참조)
+// Counter.js에서 useState 훅 함수를 사용해서 카운터의 값을 변경하도록 하고, App.js에 등록하고 랜더링된 화면을 확인해보자.
+
+// 8.1.2 useState를 여러 번 사용하기
+/**
+ * 하나의 useState 함수는 하나의 상태 값만 관리할 수 있다. 컴포넌트에서 관리해야 할 상태가 여러개라면 useState를 여러 번 사용하면 된다.
+ * src 디렉터리에 Info.js 파일을 참조
+ */
---
+++ hooks-turorial/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Info from './Info';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Info />
+      </header>
+    </div>
+  );
+}
+
+export default App;
