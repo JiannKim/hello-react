e87c9eaf74ddb2c14f28c8e1ff54a7d37c9085e3
JiannKim <jiahnkimm@gmail.com>
2021-06-02 08:26:33 +0900

    :memo: 8장 Hooks - 8.3 useReducer
    useReducer로 카운터 구현하기

--- hooks-turorial/src/App.js
+++ hooks-turorial/src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
 import React, { useState } from 'react';
-import Info from './Info';
+import Counter from './Counter';
 
 function App() {
-  const [visible, setVisible] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <button
-          onClick={() => {
-            setVisible(!visible);
-          }}
-        >
-          {visible ? '숨기기' : '보이기'}
-        </button>
-        <hr />
-        {visible && <Info />}
+        <Counter />
       </header>
     </div>
   );
