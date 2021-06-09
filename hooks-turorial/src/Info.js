b73a7d9a0c697604e6b4d7c213ef89cad58d1386
JiannKim <jiahnkimm@gmail.com>
2021-06-09 17:55:49 +0900

    :memo: 8장 Hooks - 8.3 useReducer
    useReducer를 사용해서 useState와 유사하게 작업 처리해보기

--- hooks-turorial/src/Info.js
+++ hooks-turorial/src/Info.js
@@ -1,44 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-
-  useEffect(() => {
-    // (1) 렌더링될 때 실행
-    // console.log('렌더링이 완료되었습니다!');
-    // console.log({
-    //   name,
-    //   nickname,
-    // });
-    // (2) 마운트될 때만 실행
-    // console.log('마운트될 때만 실행됩니다.');
-    // (3) 특정 값(이름/name)이 변경되어 렌더링될 때 실행
-    // console.log('name 값이 변경되었습니다!');
-    // console.log({
-    //   name,
-    // });
-    // (4) 마운트되거나 언마운트될 때마다 실행
-    console.log('effect');
-    console.log(name);
-    return () => {
-      console.log('cleanup');
-      console.log(name);
-    };
-  }, [name]);
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
+  const [state, dispatch] = useReducer(reducer, { name: '', nickname: '' });
+  console.log(state);
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
   };
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
+        <input name="name" value={name} onChange={onChange} />
         <br />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
