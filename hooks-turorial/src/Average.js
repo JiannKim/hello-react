685b2f1d6e109441e6f23c3d65ccd6710ea58f96
JiannKim <jiahnkimm@gmail.com>
2021-06-10 16:21:51 +0900

    :memo: 8장 Hooks - 8.4 useMemo
    useMemo를 사용하여 불필요한 렌더링 최소화하여 최적화

--- hooks-turorial/src/Average.js
+++ hooks-turorial/src/Average.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 
 const getAverage = (numbers) => {
-  console.log('평균값 계산시작..');
+  if (numbers.length === 0) {
+    console.log('계산할 값이 없습니다.');
+  } else {
+    console.log('평균값 계산시작..');
+  }
   if (numbers.length === 0) return 0;
   const sum = numbers.reduce((a, b) => a + b);
   console.log(sum / numbers.length, '=> 평균값');
@@ -13,8 +17,8 @@ const Average = () => {
   const [number, setNumber] = useState('');
 
   const onChange = (e) => {
-		setNumber(e.target.value);
     console.log(e.target.value, '=> 타겟값');
+    setNumber(e.target.value);
   };
   const onInsert = (e) => {
     const nextList = list.concat(parseInt(number)); //parseInt() -> 문자열 인자를 구문분석하여 특정 진수의 정수를 반환
@@ -22,6 +26,8 @@ const Average = () => {
     setList(nextList);
     setNumber(''); // input값(박스안 내용) 초기화
   };
+
+  const avg = useMemo(() => getAverage(list), [list]);
 
   return (
     <div>
@@ -35,7 +41,7 @@ const Average = () => {
 
       <div>
         <b>평균값(Average) : </b>
-        {getAverage(list)}
+        {avg}
       </div>
     </div>
   );
