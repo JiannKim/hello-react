f22fd296e1b98a8c39c34f1d1af6510c8ead8f91
JiannKim <jiahnkimm@gmail.com>
2021-06-24 13:20:34 +0900

    :memo: 8장 Hook - 8.5 useCallback
    useCallback으로 상태 값에 따라 함수를 재사용하여 렌더링 시키기

--- hooks-turorial/src/Average.js
+++ hooks-turorial/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 
 const getAverage = (numbers) => {
   if (numbers.length === 0) {
@@ -16,16 +16,17 @@ const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState('');
 
-  const onChange = (e) => {
+  const onChange = useCallback((e) => {
     console.log(e.target.value, '=> 타겟값');
     setNumber(e.target.value);
-  };
-  const onInsert = (e) => {
+  }, []); // 컴포넌트가 처음 렌더링될 때만 함수 생성
+  const onInsert = useCallback(() => {
     const nextList = list.concat(parseInt(number)); //parseInt() -> 문자열 인자를 구문분석하여 특정 진수의 정수를 반환
     console.log(nextList, '=> 리스트');
     setList(nextList);
     setNumber(''); // input값(박스안 내용) 초기화
-  };
+  }, [number, list]); // number 혹은 list가 바뀌었을때만 함수 생성
+  
   // useMemo는 memoization된 값을 반환한다. 모든 렌더링 시의 고비용 계산을 방지하게 해준다. 그리고 렌더링중에 실행되기 때문에 렌더링중에 하면 안되는일을 해당 함수 내에서 시키지않도록 주의하자
   const avg = useMemo(() => getAverage(list), [list]);
 
